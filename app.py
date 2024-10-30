1e4c9f57dd909563437671935ba1bfa5b00859b5
dat <anhcute2k4@icloud.com>
2024-10-30 13:02:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.ensemble import StackingRegressor
 from sklearn.preprocessing import StandardScaler
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Lấy cổng từ biến môi trường hoặc mặc định là 5000
+    app.run(host="0.0.0.0", port=port)
+
 # Khởi tạo ứng dụng Flask
 app = Flask(__name__)
 
